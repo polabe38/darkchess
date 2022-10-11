2c43716b56459ffb33488aef9d8e1bd82905ebce
polabe38 <115438664+polabe38@users.noreply.github.com>
2022-10-11 16:44:29 -0300

    Update ChessEngine.java

--- src/main/java/com/c0nrad/darkchess/engine/ChessEngine.java
+++ src/main/java/com/c0nrad/darkchess/engine/ChessEngine.java
@@ -189,6 +189,10 @@ public class chessEngine {
                         Position left = new Position(p.x - 1, p.y + 1);
                         if (!board.IsEmpty(left) && board.Get(left).color == Color.BLACK) {
                             possibleMoves.add(left);
+                             try {
+                        Position left = new Position(p.x - 1, p.y + 1);
+                        if (!board.IsEmpty(left) && board.Get(left).color == Color.BLACK) {
+                            possibleMoves.add(left);
                         }
                     } catch (InvalidPositionException ex) {}
 
