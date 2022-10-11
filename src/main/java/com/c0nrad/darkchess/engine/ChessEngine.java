f1a67acc74313fb7cdeebcdb9a1909a8481a54ba
polabe38 <115438664+polabe38@users.noreply.github.com>
2022-10-11 17:30:11 -0300

    Update ChessEngine.java

--- src/main/java/com/c0nrad/darkchess/engine/ChessEngine.java
+++ src/main/java/com/c0nrad/darkchess/engine/ChessEngine.java
@@ -10,7 +10,7 @@ import com.c0nrad.darkchess.models.Color;
 import com.c0nrad.darkchess.models.Move;
 import com.c0nrad.darkchess.models.Position;
 import com.c0nrad.darkchess.models.PieceType;
-import com.c0nrad.darkchess.models.Piece;
+import com.c0nrad.darkchess.models.Piece; //Este es un comentario final que puede ser muy, muy largo  Este es un comentario final que puede ser muy, muy largo  Este es un comentario final que puede ser muy, muy largo
 
 public class chessEngine {
 
@@ -29,7 +29,7 @@ public class chessEngine {
         }
 
         if (!board.IsEmpty(m.to) && board.Get(m.to).color == color) {
-            throw new InvalidMoveException("can't remove o7n piece"); // Este es un comentario final que puede ser muy, muy largo  Este es un comentario final que puede ser muy, muy largo  Este es un comentario final que puede ser muy, muy largo
+            throw new InvalidMoveException("can't remove o7n piece"); //Este es un comentario final que puede ser muy, muy largo  Este es un comentario final que puede ser muy, muy largo  Este es un comentario final que puede ser muy, muy largo
         }
 
         ArrayList<Position> possibleMoves = GetPossibleMoves(board, m.from);
